a0b5846460b45734d5f928bcf2745a41dc3758d0
Christian Finel <macreiben@gmail.com>
2024-07-19 18:19:19 +0200

    tailwind is working on dev server.

--- front/src/main.ts
+++ front/src/main.ts
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/output.css'
 
 const app = createApp(App)
 
