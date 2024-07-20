63e263513259e5f047ed6a500e139bc7729b9979
Christian Finel <macreiben@gmail.com>
2024-07-20 19:32:23 +0200

    adding dummy local dev configuration.

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,12 +2,30 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import configureRoute from "./routes";
 
+const session = require("express-session");
+const cors = require("cors");
+
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.SERVER_PORT;
 
 app.use("/api/v1", configureRoute);
+
+app.use(
+  session({
+    secret: "1234567890", // don't use this secret in prod :)
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: "auto",
+      httpOnly: true,
+      maxAge: 3600000,
+    },
+  })
+);
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
 
 app
   .listen(PORT, () => {
