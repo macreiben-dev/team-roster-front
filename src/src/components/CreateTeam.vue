af40c1811e9cedb621eecab86db451a511bbde16
Christian Finel <macreiben@gmail.com>
2024-09-04 09:33:29 +0200

    teamvue underway

--- src/src/components/CreateTeam.vue
+++ src/src/components/CreateTeam.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const teamName = ref('')
+const teamDescription = ref('')
+
+function executeCreateTeam(event: Event) {
+  event.preventDefault()
+  console.log('Creating team ...')
+  console.log('teamName:', teamName.value)
+}
+</script>
+
 <template>
-  <h1>Create Team ...</h1>
+  <div>
+    <h1>Create Team ...</h1>
+  </div>
+  <form @submit="executeCreateTeam">
+    <div class="flex flex-row">
+      <div>
+        <label for="teamName">Team Name:</label>
+        <input type="text" id="teamName" v-model="teamName" />
+      </div>
+
+      <div>
+        <label for="teamDescription">Team Description:</label>
+        <input type="text" id="teamDescription" v-model="teamDescription" />
+      </div>
+      <div>
+        <button type="submit" class="action-button">Create Team</button>
+      </div>
+    </div>
+  </form>
 </template>
