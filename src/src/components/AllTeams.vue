8828b73bc87c1a7bdf36c03a6407847296e74cd3
Christian Finel <macreiben@gmail.com>
2024-11-01 12:27:57 +0100

    display no team message when no team in store.

--- src/src/components/AllTeams.vue
+++ src/src/components/AllTeams.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
 import { useTeamRoster } from '@/stores/teamStore'
+import { computed } from 'vue'
 
 const store = useTeamRoster()
+
+const isEmpty = computed(() => store.allTeams.length === 0)
 </script>
 
 <template>
   <div>
-    <table>
-      <tr>
-        <th>Team Name</th>
-        <th>Team Description</th>
-      </tr>
-      <tr v-for="team in store.allTeams" :key="team.id">
-        <td>{{ team.name }}</td>
-      </tr>
-    </table>
+    <template v-if="isEmpty">
+      <p>... sorry, no teams created !</p>
+    </template>
+    <template v-else>
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="team in store.allTeams" :key="team.id" odd:bg-gray-100>
+            <td>{{ team.name }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
   </div>
 </template>
